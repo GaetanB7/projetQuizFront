08d0037fd7efa83bc4e309f4502f65056e79a633
Ayyoub <dawan@yopmail.com>
2022-07-18 16:52:06 +0200

    navBarre+Footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Slogan from '../components/Slogan';
+import '../style/Footer.css';
+
 
 const Home = () => {
     return (
-        <div>
-            <h2>home</h2>
+        <div className='col-md-10 m-5'>
+
+            <Slogan />
+            <div className='divFooter'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem nemo quisquam consequatur fugit, ullam ut assumenda facilis porro adipisci accusamus aspernatur sunt tempore itaque recusandae rerum. Quaerat tenetur quos aspernatur voluptatibus minus qui, dolorem dolores saepe, quod facere aliquam expedita hic, consequatur molestias ea dolore deserunt eveniet dignissimos incidunt maxime obcaecati accusantium deleniti ullam. Sequi aliquid repellat incidunt ipsa necessitatibus deleniti, perspiciatis magni libero molestiae maxime natus consequuntur beatae eveniet voluptas magnam nisi sunt tempore molestias quod omnis ipsum. Quibusdam provident molestiae harum ratione, temporibus unde accusamus. Sint dolorem error iusto porro magni! Laboriosam eveniet placeat sequi ea modi, sapiente corporis ratione! Quibusdam cumque, laboriosam accusantium soluta, consequatur at ex libero vel debitis perferendis quae architecto maiores officiis facere, earum et. Fugit, autem vel voluptates similique vitae velit doloribus laborum exercitationem ea natus officiis aperiam assumenda! Et maiores libero, modi magnam sed, aut ullam omnis, aliquam beatae eos suscipit expedita corrupti excepturi soluta? Esse reprehenderit facere mollitia error blanditiis doloribus temporibus provident optio magnam. At nobis quam nostrum non magnam consequuntur repellendus praesentium, architecto nam voluptates voluptatum, magni vero iure quaerat? Tempore aperiam tenetur asperiores placeat, adipisci officia hic reiciendis minus alias maiores quaerat harum incidunt dolorum distinctio ut nihil illum unde saepe nemo ullam odio. Suscipit praesentium ab sapiente dignissimos, iure quasi, adipisci eum accusantium quo ipsam dolorem saepe maxime autem obcaecati alias dolor perspiciatis ex animi odit voluptate atque aliquam aspernatur. Quo provident consequuntur nam, quod pariatur animi nemo expedita unde ipsam accusantium quae quibusdam neque similique obcaecati exercitationem repellat quisquam! Id magnam quaerat, obcaecati recusandae molestiae earum quo eius ex omnis laboriosam reprehenderit dignissimos doloribus consectetur aliquam assumenda itaque tempora temporibus perspiciatis. Deserunt, quibusdam ad harum modi magnam, assumenda facere distinctio quam, quos nam amet doloremque aliquam ipsum. Ut blanditiis error, ipsa fugit incidunt labore totam tempora, quam officia commodi tenetur quis accusantium laudantium excepturi aperiam voluptatum aut ullam explicabo alias est pariatur laboriosam. Sequi, eius recusandae iste optio architecto iusto vel distinctio? Illum adipisci facilis corporis odit tempore veritatis doloremque suscipit, aut libero vel, natus obcaecati delectus impedit reprehenderit eius, quo eveniet consectetur quis fugit asperiores itaque. Vero, qui. Consequuntur corporis ipsum facilis expedita est, soluta ratione modi aliquam sapiente architecto, sit, unde dicta dolores laborum repellendus in fugit. Quos perspiciatis eveniet voluptatum dolorem ad nobis quod dicta nostrum, At deserunt incidunt dicta repellat.
+            </div>
         </div>
     );
 };
